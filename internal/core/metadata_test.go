38ff4a3a8e890eb80cb046395af28c586ee34b17
ekrishgupta <ekrishgupta@users.noreply.github.com>
2026-03-05 19:28:51 -0800

    test(core): add TestTexMetadata_Basic

---
+++ internal/core/metadata_test.go
@@ -0,0 +1,40 @@
+package core
+
+import (
+	"os"
+	"testing"
+)
+
+func TestTexMetadata_Basic(t *testing.T) {
+	content := `
+\documentclass[12pt]{article}
+\usepackage{amsmath}
+\usepackage{graphicx}
+\title{Test Document}
+\author{Author Name}
+\begin{document}
+Hello world.
+\end{document}
+`
+	tmpFile, err := os.CreateTemp("", "test-*.tex")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(tmpFile.Name())
+	os.WriteFile(tmpFile.Name(), []byte(content), 0o644)
+
+	meta, err := TexMetadata(tmpFile.Name())
+	if err != nil {
+		t.Fatalf("TexMetadata failed: %v", err)
+	}
+
+	if meta.Title != "Test Document" {
+		t.Errorf("Expected title 'Test Document', got '%s'", meta.Title)
+	}
+	if meta.Author != "Author Name" {
+		t.Errorf("Expected author 'Author Name', got '%s'", meta.Author)
+	}
+	if meta.DocumentClass != "article" {
+		t.Errorf("Expected class 'article', got '%s'", meta.DocumentClass)
+	}
+}
